7ccb040cb406519c80a79f983dfecefc2b288b77
Kamilhan KARAİSMAİLOĞLU <k_karaismailoglu@aof.anadolu.edu.tr>
2021-05-29 01:19:00 +0300

    Proje dosyası ekle.

---
+++ Puzzle/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿
+namespace Puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.NewGame = new System.Windows.Forms.Button();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel = new System.Windows.Forms.Panel();
+            this.buttonKontrol = new System.Windows.Forms.Button();
+            this.trackBarIpucu = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelSüre = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.buttonBitir = new System.Windows.Forms.Button();
+            this.timerKapat = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblBaslik = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarIpucu)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NewGame
+            // 
+            this.NewGame.Location = new System.Drawing.Point(12, 138);
+            this.NewGame.Name = "NewGame";
+            this.NewGame.Size = new System.Drawing.Size(108, 34);
+            this.NewGame.TabIndex = 1;
+            this.NewGame.Text = "YENİ OYUN";
+            this.NewGame.UseVisualStyleBackColor = true;
+            this.NewGame.Click += new System.EventHandler(this.NewGame_Click);
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(171, 22);
+            this.textBoxWidth.MaxLength = 2;
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(56, 20);
+            this.textBoxWidth.TabIndex = 0;
+            this.textBoxWidth.Text = "5";
+            this.textBoxWidth.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxWidth.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxWidth_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "SATIR ELEMAN SAYISI :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "( N x N Formatında )";
+            // 
+            // panel
+            // 
+            this.panel.AutoSize = true;
+            this.panel.BackColor = System.Drawing.SystemColors.Control;
+            this.panel.Location = new System.Drawing.Point(277, 39);
+            this.panel.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(270, 177);
+            this.panel.TabIndex = 12;
+            // 
+            // buttonKontrol
+            // 
+            this.buttonKontrol.Location = new System.Drawing.Point(132, 138);
+            this.buttonKontrol.Name = "buttonKontrol";
+            this.buttonKontrol.Size = new System.Drawing.Size(108, 34);
+            this.buttonKontrol.TabIndex = 13;
+            this.buttonKontrol.Text = "KONTROL ET";
+            this.buttonKontrol.UseVisualStyleBackColor = true;
+            this.buttonKontrol.Click += new System.EventHandler(this.buttonKontrol_Click);
+            // 
+            // trackBarIpucu
+            // 
+            this.trackBarIpucu.LargeChange = 1;
+            this.trackBarIpucu.Location = new System.Drawing.Point(141, 67);
+            this.trackBarIpucu.Name = "trackBarIpucu";
+            this.trackBarIpucu.Size = new System.Drawing.Size(92, 45);
+            this.trackBarIpucu.TabIndex = 14;
+            this.trackBarIpucu.TickFrequency = 5;
+            this.trackBarIpucu.Value = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "ZORLUK SEVİYESİ :";
+            // 
+            // labelSüre
+            // 
+            this.labelSüre.AutoSize = true;
+            this.labelSüre.Location = new System.Drawing.Point(9, 234);
+            this.labelSüre.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
+            this.labelSüre.Name = "labelSüre";
+            this.labelSüre.Size = new System.Drawing.Size(87, 13);
+            this.labelSüre.TabIndex = 16;
+            this.labelSüre.Text = "OYUN SÜRESİ :";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(89, 234);
+            this.lblTime.Margin = new System.Windows.Forms.Padding(3, 0, 3, 10);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(49, 13);
+            this.lblTime.TabIndex = 17;
+            this.lblTime.Text = "00:00:00";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // buttonBitir
+            // 
+            this.buttonBitir.Location = new System.Drawing.Point(69, 178);
+            this.buttonBitir.Name = "buttonBitir";
+            this.buttonBitir.Size = new System.Drawing.Size(108, 34);
+            this.buttonBitir.TabIndex = 18;
+            this.buttonBitir.Text = "OYUNU SONLANDIR";
+            this.buttonBitir.UseVisualStyleBackColor = true;
+            this.buttonBitir.Click += new System.EventHandler(this.buttonBitir_Click);
+            // 
+            // timerKapat
+            // 
+            this.timerKapat.Interval = 1000;
+            this.timerKapat.Tick += new System.EventHandler(this.timerKapat_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(133, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "KOLAY";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(171, 89);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 12);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "ORTA";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(208, 89);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 12);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "ZOR";
+            // 
+            // lblBaslik
+            // 
+            this.lblBaslik.AutoSize = true;
+            this.lblBaslik.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBaslik.Location = new System.Drawing.Point(166, 9);
+            this.lblBaslik.Name = "lblBaslik";
+            this.lblBaslik.Size = new System.Drawing.Size(208, 13);
+            this.lblBaslik.TabIndex = 19;
+            this.lblBaslik.Text = "PUZZLE OYUNUNA HOŞGELDİNİZ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.lblTime);
+            this.groupBox1.Controls.Add(this.NewGame);
+            this.groupBox1.Controls.Add(this.labelSüre);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBoxWidth);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.buttonKontrol);
+            this.groupBox1.Controls.Add(this.buttonBitir);
+            this.groupBox1.Controls.Add(this.trackBarIpucu);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(10, 30);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(251, 254);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(555, 296);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblBaslik);
+            this.Controls.Add(this.panel);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Puzzle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarIpucu)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button NewGame;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button buttonKontrol;
+        private System.Windows.Forms.TrackBar trackBarIpucu;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelSüre;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button buttonBitir;
+        private System.Windows.Forms.Timer timerKapat;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblBaslik;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Puzzle/Form1.cs
@@ -0,0 +1,421 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Puzzle
+{
+    class cell : TextBox
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+    class ipucu : Label
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+    public partial class Form1 : Form
+    {
+        static int[,] CevapAnahtarı;
+        static int genislik;
+        static List<cell> txtBoxlar = new List<cell> { };
+        static List<ipucu> ipuclari = new List<ipucu> { };
+        public Form1()
+        {
+            InitializeComponent();
+            trackBarIpucu.Maximum = 3;
+            trackBarIpucu.Minimum = 1;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CevapAnahtarı = CevapAnahtarıOlustur();
+
+            while (!tabloKontrol(CevapAnahtarı))
+            {
+                CevapAnahtarı = CevapAnahtarıOlustur();
+            }
+            PanelOlustur();
+            if (durum == 0)
+            {
+                durum = 1;
+                timer.Start();
+            }
+            lblBaslik.Location = new Point((panel.Width + groupBox1.Width) / 3, 9);
+        }
+        private void textBoxWidth_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+        private void NewGame_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(textBoxWidth.Text)>8 || Convert.ToInt32(textBoxWidth.Text) < 2)
+            {
+                MessageBox.Show("Izgara İçin En Fazla 8x8, En Az 2x2 Büyüklüğünde Olucak Şekilde n Değeri Giriniz", "Tablo Oluşturulamadı");
+            }
+            else
+            {
+                txtBoxlar.Clear();
+                ipuclari.Clear();
+                panel.Controls.Clear();
+                CevapAnahtarı = CevapAnahtarıOlustur();
+
+                timer.Enabled = true;
+                buttonKontrol.Enabled = true;
+                while (!tabloKontrol(CevapAnahtarı))
+                {
+                    CevapAnahtarı = CevapAnahtarıOlustur();
+                }
+
+
+                PanelOlustur();
+
+                if (Convert.ToInt32(textBoxWidth.Text) > 4)
+                {
+                    this.Size = new Size(panel.Width + 270, panel.Height+100);
+
+                }
+                else if (Convert.ToInt32(textBoxWidth.Text) <= 4)
+                {
+                    this.Size = new Size(panel.Width + 270, 301);
+                }
+
+                saat = 0;
+                dakika = 0;
+                saniye = 0;
+                if (durum == 0)
+                {
+                    durum = 1;
+                    timer.Start();
+                }
+
+                lblBaslik.Location = new Point((panel.Width+groupBox1.Width)/3, 9);
+            }
+        }
+        int[,] CevapAnahtarıOlustur()
+        {
+            int gelenGenislik = Convert.ToInt16(textBoxWidth.Text);
+            genislik = gelenGenislik;
+            Random rnd = new Random();
+           
+            int[,] tablo = new int[genislik, genislik];
+
+            int RandomDeger = rnd.Next(1, genislik + 1);
+            int sayac = 0;
+
+            for (int x = 0; x < genislik; x++)
+            {
+                List<int> satırDegerleri = new List<int> { };
+                for (int i = 1; i < genislik+1; i++)
+                {
+                    satırDegerleri.Add(i);
+                }
+
+                for (int y = 0; y < genislik; y++)
+                {
+                    sayac = 0;
+                    while (VarMı(RandomDeger, x, y, tablo)==false)
+                    {
+                        if (sayac>genislik)
+                        {
+                            RandomDeger = 0;
+                            break;
+                        }
+                        RandomDeger = satırDegerleri[rnd.Next(0,satırDegerleri.Count)];
+                        sayac++;
+                    }
+                    tablo[x, y] = RandomDeger;
+                    satırDegerleri.Remove(RandomDeger);
+                }
+            }
+            
+            return tablo;          
+        }
+        bool tabloKontrol(int[,] tablo)
+        {
+            foreach (var item in tablo)
+            {
+                if (item == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        bool VarMı(int randomDeger, int x, int y, int[,] tablo)
+        {
+            for (int i = 0; i < genislik; i++)
+            {            
+                if (i!=x && tablo[i, y] == randomDeger)
+                {
+                    return false;
+                }
+                
+                if (i != y && tablo[x, i] == randomDeger)
+                {
+                    return false;
+                }             
+            }
+            return true;
+        }
+        void PanelOlustur() 
+        {
+            Random rnd = new Random();
+            panel.Enabled = true;
+            panel.Size = new Size(genislik*60,genislik*50-30);
+            int sayac=0;
+
+            for (int x = 0; x < genislik*60; x=x+60)
+            {
+                for (int y = 0; y < genislik * 50; y = y + 50)
+                {
+                    textBoxEkle(new Point(x+11, y));
+                    
+                      if (x != genislik * 60 - 60)
+                      {
+                            
+                            AraLabelEkle(new Point(x, y));
+                            sayac++;
+                      }
+                      if (y != genislik * 50 - 50)
+                      {
+                            
+                            SatırLabelEkle(new Point(x - 30, y + 25));
+                            sayac++;                     
+                      }                                       
+                }
+            }
+
+            int sınır = 0;
+            int anlıkTotal = 0;
+
+            if (trackBarIpucu.Value == 1)
+            {
+                sınır = (genislik-1) * (genislik-1)*2;
+                if (genislik == 2)
+                {
+                    sınır = 3;
+                }
+            }
+            else if (trackBarIpucu.Value == 2)
+            {
+                sınır = genislik * (genislik-2)+5;
+                if (genislik < 4)
+                {
+                    sınır = genislik+2;
+                }
+                if (genislik==2)
+                {
+                    sınır = 2;
+                }
+            }
+            else if (trackBarIpucu.Value == 3)
+            {
+                sınır = genislik*(genislik-3);
+                if (genislik<4)
+                {
+                    sınır = genislik;
+                }
+                if (genislik == 2)
+                {
+                    sınır = 1;
+                }
+            }
+
+            int random;
+            while (anlıkTotal != sınır)
+            {
+                random = rnd.Next(0, ipuclari.Count);
+                if (ipuclari[random].Visible == false)
+                {
+                    ipuclari[random].Visible = true;
+                    anlıkTotal++;
+                }
+            }
+        }
+        void textBoxEkle(Point konum)
+        {
+            cell txtBox = new cell();
+            txtBox.Name = "txtBox"+((konum.X-11)/60).ToString()+ (konum.Y/50).ToString();
+            txtBox.Size = new Size(30, 30);
+            txtBox.Location = konum;
+            txtBox.TextAlign = HorizontalAlignment.Center;
+            txtBox.KeyPress += textBoxWidth_KeyPress;
+            txtBox.X = konum.X;
+            txtBox.Y = konum.Y;
+            txtBoxlar.Add(txtBox);
+            panel.Controls.Add(txtBox);
+        }
+        void AraLabelEkle(Point konum)
+        {
+            ipucu lbl = new ipucu();
+            lbl.Name = "lbl:"+konum.X + "," + konum.Y;
+            lbl.Location = new Point(konum.X+50,konum.Y);
+            lbl.Width = 10;
+            lbl.Visible = false;
+            lbl.X = konum.X;
+            lbl.Y = konum.Y;
+            if (CevapAnahtarı[lbl.Y / 50, lbl.X / 60] > CevapAnahtarı[lbl.Y / 50, lbl.X / 60 + 1])
+            {
+                lbl.Text = ">";
+            }
+            else if (CevapAnahtarı[lbl.Y / 50, lbl.X / 60] < CevapAnahtarı[lbl.Y / 50, lbl.X / 60 + 1])
+            {
+                lbl.Text = "<";
+            }
+            ipuclari.Add(lbl);
+            panel.Controls.Add(lbl);
+        }
+        void SatırLabelEkle(Point konum)
+        {
+            ipucu lbl = new ipucu();
+            lbl.Name = "lbl:" + konum.X + "," + konum.Y;
+            lbl.Location = new Point(konum.X + 49, konum.Y);
+            lbl.Width = 10;
+            lbl.Height = 15;
+            lbl.Visible = false;
+            lbl.X = konum.X;
+            lbl.Y = konum.Y;
+            if (CevapAnahtarı[(lbl.Y-25) / 50, (lbl.X +30) / 60] > CevapAnahtarı[(lbl.Y - 25)/ 50+1, (lbl.X + 30) / 60])
+            {
+                lbl.Text = "∨";
+            }
+            else if (CevapAnahtarı[(lbl.Y - 25) / 50, (lbl.X + 30) / 60] < CevapAnahtarı[(lbl.Y - 25) / 50+1, (lbl.X + 30) / 60])
+            {
+                lbl.Text = "∧";
+            }
+            ipuclari.Add(lbl);
+            panel.Controls.Add(lbl);
+        }
+        private void buttonKontrol_Click(object sender, EventArgs e)
+        {   
+            cell[,] textboxMatris = new cell[genislik, genislik];
+            int sayac = 0;
+            for (int x = 0; x < genislik; x++)
+            {
+                for (int y = 0; y < genislik; y++)
+                {
+                    textboxMatris[y, x] = txtBoxlar[sayac];
+                    sayac++;
+                }
+            }
+            
+            int[,] CevapMatrisi = new int[genislik, genislik];
+            for (int x = 0; x < genislik; x++)
+            {
+                for (int y = 0; y < genislik; y++)
+                {
+                    if (textboxMatris[x, y].Text != null && textboxMatris[x, y].Text != "")
+                    {
+                        CevapMatrisi[x, y] = Convert.ToInt32(textboxMatris[x, y].Text);
+                    }
+                    else
+                    {
+                        CevapMatrisi[x, y] = 0;
+                    }
+                }
+            }
+
+            int hatasayısı = 0;
+            for (int x = 0; x < genislik; x++)
+            {
+                for (int y = 0; y < genislik; y++)
+                {
+                    textboxMatris[x, y].BackColor = Color.White;
+                    textboxMatris[x, y].ForeColor = Color.White;
+
+                    if (CevapMatrisi[x, y] != CevapAnahtarı[x, y])
+                    {
+                        textboxMatris[x, y].BackColor = Color.Red;
+                        hatasayısı++;
+                    }
+                    else
+                    {
+                        textboxMatris[x, y].BackColor = Color.Green;
+                    }
+                }
+            }
+            if (hatasayısı==0)
+            {
+                panel.Enabled = false;
+                buttonKontrol.Enabled = false;
+                timer.Enabled = false;
+                for (int x = 0; x < genislik; x++)
+                {
+                    for (int y = 0; y < genislik; y++)
+                    {
+                        textboxMatris[x, y].BackColor = Color.White;
+                        textboxMatris[x, y].ForeColor = Color.Black;                   
+                    }
+                }
+                MessageBox.Show("Tebrikler Kazandınız","Oyun Bitti");
+            }
+        }
+
+        int durum = 0;
+        int saat = 0, dakika = 0, saniye = 0;
+        int sayacKapat = 0;
+        private void timerKapat_Tick(object sender, EventArgs e)
+        {
+            sayacKapat++;
+            if (sayacKapat == 4)
+            {
+                Application.Exit();
+            }
+        }
+        private void buttonBitir_Click(object sender, EventArgs e)
+        {
+            cell[,] textboxMatris = new cell[genislik, genislik];
+            int sayac = 0;
+            for (int x = 0; x < genislik; x++)
+            {
+                for (int y = 0; y < genislik; y++)
+                {
+                    textboxMatris[y, x] = txtBoxlar[sayac];
+                    sayac++;
+                }
+            }
+
+            for (int x = 0; x < genislik; x++)
+            {
+                for (int y = 0; y < genislik; y++)
+                {
+                    textboxMatris[x, y].BackColor = Color.White;
+                    textboxMatris[x, y].Text = CevapAnahtarı[x, y].ToString();                
+                }
+            }
+
+            panel.Enabled = false;
+            buttonBitir.Enabled = false;
+            buttonKontrol.Enabled = false;
+            NewGame.Enabled = false;
+            trackBarIpucu.Enabled = false;
+            textBoxWidth.Enabled = false;
+            timer.Enabled = false;
+
+            lblBaslik.Text = "OYUN SONLANDIRILIYOR...";
+            timerKapat.Enabled = true;
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (saniye == 60)
+            {
+                saniye = 0;
+                dakika++;
+            }
+            if (dakika == 60)
+            {
+                dakika = 0;
+                saniye = 0;
+                saat++;
+            }
+            lblTime.Text = String.Format("{0:D2}", saat) + ":" + String.Format("{0:D2}", dakika) + ":" + String.Format("{0:D2}", saniye);
+            saniye++;
+        }
+    }
+}
